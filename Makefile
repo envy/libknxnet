3cbf960e88a5223bc8c27df5a456d6058b7b6e1d
Nico Weichbrodt <nico@weichbrodt.me>
2018-06-17 13:34:05 +0200

    Added conversion

--- Makefile
+++ Makefile
@@ -1,12 +1,16 @@
-CXXFLAGS += -Wall -Wextra -std=c++11
+CXXFLAGS += -Wall -Wextra -std=c++11 -fpic -fstack-protector-strong -march=native -O2 -g
+LDFLAGS += -shared
 
-all: libknxnet.a
+all: libknxnet.a libknxnet.so
 
 libknxnet.a: knxnet.o
 	ar r $@ $^
+
+libknxnet.so: knxnet.o
+	$(CXX) $(LDFLAGS) $^ -o $@
 
 knxnet.o: knxnet.cpp knxnet.h
 	$(CXX) $(CXXFLAGS) -c -o $@ $<
 
 clean:
-	rm -rf *.o *.a
+	rm -rf *.o *.a *.so
--- knxnet.h
+++ knxnet.h
@@ -235,6 +235,24 @@ namespace knxnet
 
 	typedef void(*callback_t)(message_t &msg);
 
+	/**
+	 * Conversion functions
+	 */
+	bool data_to_bool(uint8_t *data);
+	int8_t data_to_1byte_int(uint8_t *data);
+	uint8_t data_to_1byte_uint(uint8_t *data);
+	int16_t data_to_2byte_int(uint8_t *data);
+	uint16_t data_to_2byte_uint(uint8_t *data);
+	float data_to_2byte_float(uint8_t *data);
+	/*
+	time_of_day_t data_to_3byte_time(uint8_t *data)
+	date_t data_to_3byte_data(uint8_t *data)
+	color_t data_to_3byte_color(uint8_t *data)
+	*/
+	int32_t data_to_4byte_int(uint8_t *data);
+	uint32_t data_to_4byte_uint(uint8_t *data);
+	float data_to_4byte_float(uint8_t *data);
+
 	class KNXnet
 	{
 	public:
