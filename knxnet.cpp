b8aa08f0f55fff1a4916a2c2efaaa49c932395fa
Nico Weichbrodt <nico@weichbrodt.me>
2018-12-24 22:53:17 +0100

    consistent indentation

--- knxnet.cpp
+++ knxnet.cpp
@@ -17,7 +17,7 @@ knxnet::KNXnet::KNXnet(const char *interface_ip, address_t &physical_addr)
 		std::cerr << "Error opening receviving socket: " << std::strerror(errno) << std::endl;
 		throw new std::exception();
 	}
-	
+
 	int tmp = 1;
 	if (setsockopt(socket_recv_fd, SOL_SOCKET, SO_REUSEADDR, &tmp, sizeof(tmp)) < 0)
 	{
@@ -92,25 +92,25 @@ void knxnet::KNXnet::receive(callback_t callback_func)
 		throw new std::exception();
 	}
 
-    knx_ip_pkt_t *knx_pkt = (knx_ip_pkt_t *)buf;
+	knx_ip_pkt_t *knx_pkt = (knx_ip_pkt_t *)buf;
 
-    if (knx_pkt->header_len != 0x06 && knx_pkt->protocol_version != 0x10 && knx_pkt->service_type != KNX_ST_ROUTING_INDICATION)
-        return;
+	if (knx_pkt->header_len != 0x06 && knx_pkt->protocol_version != 0x10 && knx_pkt->service_type != KNX_ST_ROUTING_INDICATION)
+		return;
 
-    cemi_msg_t *cemi_msg = (cemi_msg_t *)knx_pkt->pkt_data;
+	cemi_msg_t *cemi_msg = (cemi_msg_t *)knx_pkt->pkt_data;
 
-    if (cemi_msg->message_code != KNX_MT_L_DATA_IND)
-        return;
+	if (cemi_msg->message_code != KNX_MT_L_DATA_IND)
+		return;
 
-    cemi_service_t *cemi_data = &cemi_msg->data.service_information;
+	cemi_service_t *cemi_data = &cemi_msg->data.service_information;
 
-    if (cemi_msg->additional_info_len > 0)
-        cemi_data = (cemi_service_t *)(((uint8_t *)cemi_data) + cemi_msg->additional_info_len);
+	if (cemi_msg->additional_info_len > 0)
+		cemi_data = (cemi_service_t *)(((uint8_t *)cemi_data) + cemi_msg->additional_info_len);
 
-    if (cemi_data->control_2.bits.dest_addr_type != 0x01)
-        return;
+	if (cemi_data->control_2.bits.dest_addr_type != 0x01)
+		return;
 
-    knx_command_type_t ct = (knx_command_type_t)(((cemi_data->data[0] & 0xC0) >> 6) | ((cemi_data->pci.apci & 0x03) << 2));
+	knx_command_type_t ct = (knx_command_type_t)(((cemi_data->data[0] & 0xC0) >> 6) | ((cemi_data->pci.apci & 0x03) << 2));
 
 	uint8_t data[cemi_data->data_len];
 	memcpy(data, cemi_data->data, cemi_data->data_len);
@@ -128,48 +128,48 @@ void knxnet::KNXnet::receive(callback_t callback_func)
 
 void knxnet::KNXnet::send(message_t &msg)
 {
-    uint32_t len = 6 + 2 + 8 + msg.data_len; // knx_pkt + cemi_msg + cemi_service + data
-    uint8_t buf[len];
-    knx_ip_pkt_t *knx_pkt = (knx_ip_pkt_t *)buf;
-    knx_pkt->header_len = 0x06;
-    knx_pkt->protocol_version = 0x10;
-    knx_pkt->service_type = htons(KNX_ST_ROUTING_INDICATION);
-    knx_pkt->total_len.len = htons(len);
-    cemi_msg_t *cemi_msg = (cemi_msg_t *)knx_pkt->pkt_data;
-    cemi_msg->message_code = KNX_MT_L_DATA_IND;
-    cemi_msg->additional_info_len = 0;
-    cemi_service_t *cemi_data = &cemi_msg->data.service_information;
-    cemi_data->control_1.bits.confirm = 0;
-    cemi_data->control_1.bits.ack = 0;
-    cemi_data->control_1.bits.priority = 0b11;
-    cemi_data->control_1.bits.system_broadcast = 0x01;
-    cemi_data->control_1.bits.repeat = 0x01;
-    cemi_data->control_1.bits.reserved = 0;
-    cemi_data->control_1.bits.frame_type = 0x01;
-    cemi_data->control_2.bits.extended_frame_format = 0x00;
-    cemi_data->control_2.bits.hop_count = 0x06;
-    cemi_data->control_2.bits.dest_addr_type = 0x01;
-    cemi_data->source = msg.sender;
-    cemi_data->destination = msg.receiver;
-    //cemi_data->destination.bytes.high = (area << 3) | line;
-    //cemi_data->destination.bytes.low = member;
-    cemi_data->data_len = msg.data_len;
-    cemi_data->pci.apci = (msg.ct & 0x0C) >> 2;
-    cemi_data->pci.tpci_seq_number = 0x00; // ???
-    cemi_data->pci.tpci_comm_type = KNX_COT_UDP; // ???
-    memcpy(cemi_data->data, msg.data, msg.data_len);
-    cemi_data->data[0] = (cemi_data->data[0] & 0x3F) | ((msg.ct & 0x03) << 6);
+	uint32_t len = 6 + 2 + 8 + msg.data_len; // knx_pkt + cemi_msg + cemi_service + data
+	uint8_t buf[len];
+	knx_ip_pkt_t *knx_pkt = (knx_ip_pkt_t *)buf;
+	knx_pkt->header_len = 0x06;
+	knx_pkt->protocol_version = 0x10;
+	knx_pkt->service_type = htons(KNX_ST_ROUTING_INDICATION);
+	knx_pkt->total_len.len = htons(len);
+	cemi_msg_t *cemi_msg = (cemi_msg_t *)knx_pkt->pkt_data;
+	cemi_msg->message_code = KNX_MT_L_DATA_IND;
+	cemi_msg->additional_info_len = 0;
+	cemi_service_t *cemi_data = &cemi_msg->data.service_information;
+	cemi_data->control_1.bits.confirm = 0;
+	cemi_data->control_1.bits.ack = 0;
+	cemi_data->control_1.bits.priority = 0b11;
+	cemi_data->control_1.bits.system_broadcast = 0x01;
+	cemi_data->control_1.bits.repeat = 0x01;
+	cemi_data->control_1.bits.reserved = 0;
+	cemi_data->control_1.bits.frame_type = 0x01;
+	cemi_data->control_2.bits.extended_frame_format = 0x00;
+	cemi_data->control_2.bits.hop_count = 0x06;
+	cemi_data->control_2.bits.dest_addr_type = 0x01;
+	cemi_data->source = msg.sender;
+	cemi_data->destination = msg.receiver;
+	//cemi_data->destination.bytes.high = (area << 3) | line;
+	//cemi_data->destination.bytes.low = member;
+	cemi_data->data_len = msg.data_len;
+	cemi_data->pci.apci = (msg.ct & 0x0C) >> 2;
+	cemi_data->pci.tpci_seq_number = 0x00; // ???
+	cemi_data->pci.tpci_comm_type = KNX_COT_UDP; // ???
+	memcpy(cemi_data->data, msg.data, msg.data_len);
+	cemi_data->data[0] = (cemi_data->data[0] & 0x3F) | ((msg.ct & 0x03) << 6);
 
-    struct sockaddr_in address = {};
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = inet_addr(MULTICAST_IP);
-    address.sin_port = htons(MULTICAST_PORT);
+	struct sockaddr_in address = {};
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = inet_addr(MULTICAST_IP);
+	address.sin_port = htons(MULTICAST_PORT);
 
-    if (sendto(socket_send_fd, buf, len, 0, (struct sockaddr *)&address, sizeof(address)) < 0)
-    {
+	if (sendto(socket_send_fd, buf, len, 0, (struct sockaddr *)&address, sizeof(address)) < 0)
+	{
 		std::cerr << "Error sending: " << std::strerror(errno) << std::endl;
 		throw new std::exception();
-    }	
+	}
 }
 
 /**
@@ -226,30 +226,30 @@ float knxnet::data_to_2byte_float(uint8_t *data)
 /*
 time_of_day_t knxnet::data_to_3byte_time(uint8_t *data)
 {
-    time_of_day_t time;
-    time.weekday = (weekday_t)((data[1] & 0b11100000) >> 5);
-    time.hours = (data[1] & 0b00011111);
-    time.minutes = (data[2] & 0b00111111);
-    time.seconds = (data[3] & 0b00111111);
-    return time;
+	time_of_day_t time;
+	time.weekday = (weekday_t)((data[1] & 0b11100000) >> 5);
+	time.hours = (data[1] & 0b00011111);
+	time.minutes = (data[2] & 0b00111111);
+	time.seconds = (data[3] & 0b00111111);
+	return time;
 }
 
 date_t knxnet::data_to_3byte_data(uint8_t *data)
 {
-    date_t date;
-    date.day = (data[1] & 0b00011111);
-    date.month = (data[2] & 0b00001111);
-    date.year = (data[3] & 0b01111111);
-    return date;
+	date_t date;
+	date.day = (data[1] & 0b00011111);
+	date.month = (data[2] & 0b00001111);
+	date.year = (data[3] & 0b01111111);
+	return date;
 }
 
 color_t knxnet::data_to_3byte_color(uint8_t *data)
 {
-    color_t color;
-    color.red = data[1];
-    color.green = data[2];
-    color.blue = data[3];
-    return color;
+	color_t color;
+	color.red = data[1];
+	color.green = data[2];
+	color.blue = data[3];
+	return color;
 }
 */
 
